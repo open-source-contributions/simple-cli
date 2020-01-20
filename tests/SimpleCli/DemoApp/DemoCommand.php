773652f6729b38c671d8836dd68af6fe4c3766b5
peter279k <peter279k@gmail.com>
2020-01-20 10:44:48 +0000

    Apply fixes from StyleCI

--- tests/SimpleCli/DemoApp/DemoCommand.php
+++ tests/SimpleCli/DemoApp/DemoCommand.php
@@ -12,8 +12,8 @@ use SimpleCli\SimpleCli;
  */
 class DemoCommand implements Command
 {
-    use Verbose, Help;
-
+    use Verbose;
+    use Help;
     /**
      * @option
      * @values hello, hi, bye
